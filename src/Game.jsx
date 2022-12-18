db1db252f75bcd6cbd0eff3a0313cdc4c4fa17d9
jaasonw <jayywong92@gmail.com>
2022-12-17 19:05:41 -0800

    fix reference to gameid

--- src/Game.jsx
+++ src/Game.jsx
@@ -8,13 +8,13 @@ import "./index.css";
 import { checkWin, create2d, index1d, isFilled } from "./util";
 
 const pb = new PocketBase("https://cpsc349project4.fly.dev");
-const id = new URLSearchParams(window.location.search).get("gameId") ?? "";
+const gameId = new URLSearchParams(window.location.search).get("gameId");
 // console.log(pb.authStore.isValid);
 // console.log(pb.authStore.token);
 // console.log(pb.authStore.model.id);
 // console.log(pb.authStore.model);
 
-export function Game(gameId = id) {
+export function Game() {
   const [board, setBoard] = useState(create2d(3));
   const [turn, setTurn] = useState("X");
   const [winner, setWinner] = useState(null);
